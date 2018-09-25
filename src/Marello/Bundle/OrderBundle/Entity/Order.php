5458a5cafac94f81e9b31c14659fd3e841fd7aef
Kadir Yucel (Ubuntu) <ky@clicktrend.de>
2018-09-25 11:41:58 +0200

    Add order as context in my emails

--- src/Marello/Bundle/OrderBundle/Entity/Order.php
+++ src/Marello/Bundle/OrderBundle/Entity/Order.php
@@ -49,6 +49,10 @@ use Symfony\Component\Validator\Context\ExecutionContextInterface;
  *          },
  *          "dataaudit"={
  *              "auditable"=true
+ *          },
+ *          "grid"={
+ *              "default"="order-grid",
+ *              "context"="order-for-context-grid"
  *          }
  *      }
  * )
