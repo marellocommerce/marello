fdb8d273093e063d08f2976b4f54f48643b486b6
Hotlander <j.casteleijn@labelgoods.nl>
2024-03-27 10:10:53 +0100

    feature/MAR10001-1490: - reworked additional response data for configuration of SalesChannel - removed compiler pass of additional scope

--- src/Marello/Bundle/SalesBundle/MarelloSalesBundle.php
+++ src/Marello/Bundle/SalesBundle/MarelloSalesBundle.php
@@ -2,16 +2,8 @@
 
 namespace Marello\Bundle\SalesBundle;
 
-use Marello\Bundle\SalesBundle\DependencyInjection\CompilerPass\SalesChannelApiConfigScopePass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MarelloSalesBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new SalesChannelApiConfigScopePass());
-    }
 }
