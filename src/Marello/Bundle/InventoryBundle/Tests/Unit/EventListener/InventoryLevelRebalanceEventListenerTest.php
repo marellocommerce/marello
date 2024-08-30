dff60e68ddf5fe12978d9d6ab954bfc816cd33cc
Hotlander <j.casteleijn@labelgoods.nl>
2024-08-30 10:45:59 +0200

    task/MAR10001-1506-oro-update-6-0-0: - Added test for InventoryLevelRebalanceEventListener

--- src/Marello/Bundle/InventoryBundle/Tests/Unit/EventListener/InventoryLevelRebalanceEventListenerTest.php
+++ src/Marello/Bundle/InventoryBundle/Tests/Unit/EventListener/InventoryLevelRebalanceEventListenerTest.php
@@ -4,53 +4,146 @@ namespace Marello\Bundle\InventoryBundle\Tests\Unit\EventListener;
 
 use PHPUnit\Framework\TestCase;
 
-use Marello\Bundle\WebhookBundle\Manager\WebhookProducer;
-use Marello\Bundle\InventoryBundle\Manager\InventoryManager;
-use Marello\Bundle\InventoryBundle\Event\InventoryUpdateEvent;
-use Marello\Bundle\InventoryBundle\Model\InventoryUpdateContext;
-use Marello\Bundle\InventoryBundle\Manager\BalancedInventoryManager;
-use Marello\Bundle\InventoryBundle\Event\InventoryUpdateWebhookEvent;
-use Marello\Bundle\InventoryBundle\EventListener\InventoryUpdateEventListener;
+use Doctrine\ORM\UnitOfWork;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
+use Oro\Bundle\MessageQueueBundle\Client\BufferedMessageProducer;
+
+use Marello\Bundle\ProductBundle\Entity\Product;
+use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
+use Marello\Bundle\InventoryBundle\Entity\InventoryLevel;
+use Marello\Bundle\InventoryBundle\Manager\InventoryItemManagerInterface;
+use Marello\Bundle\InventoryBundle\Async\Topic\ResolveRebalanceInventoryTopic;
+use Marello\Bundle\InventoryBundle\EventListener\Doctrine\InventoryLevelRebalanceEventListener;
 
 class InventoryLevelRebalanceEventListenerTest extends TestCase
 {
     /**
-     * @var InventoryUpdateContext|\PHPUnit\Framework\MockObject\MockObject
-     */
-    protected $inventoryUpdateContext;
-
-    /**
-     * @var InventoryUpdateEventListener
+     * @var InventoryLevelRebalanceEventListener
      */
     protected $listener;
 
     /**
-     * @var InventoryManager|\PHPUnit\Framework\MockObject\MockObject
+     * @var InventoryItemManagerInterface|\PHPUnit\Framework\MockObject\MockObject
      */
-    protected $inventoryManager;
+    protected $inventoryItemManager;
 
     /**
-     * @var BalancedInventoryManager|\PHPUnit\Framework\MockObject\MockObject
+     * @var EntityManagerInterface|\PHPUnit\Framework\MockObject\MockObject
      */
-    protected $balancedInventoryManager;
+    protected $entityManager;
 
     /**
-     * @var WebhookProducer|\PHPUnit\Framework\MockObject\MockObject
+     * @var MessageProducerInterface|\PHPUnit\Framework\MockObject\MockObject
      */
-    protected $webhookProducer;
+    protected $producer;
 
     /**
      * {@inheritdoc}
      */
     public function setUp(): void
     {
-        // todo:
-        // create test(s) that will cover the inventory level update and trigger of the balancer
-        // check that it does cover both the update as the trigger for the balancer.
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->producer = $this->createMock(BufferedMessageProducer::class);
+
+        $this->listener = new InventoryLevelRebalanceEventListener($this->producer);
     }
 
-    public function testHandleUpdateInventoryEvent()
+    public function testProductInventoryIsRebalancedAfterCreation()
     {
+        $event = $this->prepareEvent();
+        $uowMock = $this->createMock(UnitOfWork::class);
 
+        $this->entityManager->expects($this->atLeastOnce())
+            ->method('getUnitOfWork')
+            ->willReturn($uowMock);
+
+        $inventoryLevelMock = $this->createMock(InventoryLevel::class);
+        $inventoryItemMock = $this->createMock(InventoryItem::class);
+        $productMock = $this->createMock(Product::class);
+
+
+        $uowMock->expects($this->atLeastOnce())
+            ->method('getScheduledEntityInsertions')
+            ->willReturn([$inventoryLevelMock]);
+
+        $uowMock->expects($this->atLeastOnce())
+            ->method('getScheduledEntityUpdates')
+            ->willReturn([]);
+
+        $inventoryLevelMock->expects($this->once())
+            ->method('getInventoryItem')
+            ->willReturn($inventoryItemMock);
+
+        $inventoryItemMock->expects($this->once())
+            ->method('getProduct')
+            ->willReturn($productMock);
+
+        $productMock->expects($this->exactly(2))
+            ->method('getId')
+            ->willReturn(1);
+
+        $this->producer->expects($this->once())
+            ->method('enableBuffering');
+
+        $this->producer->expects($this->once())
+            ->method('send')
+            ->with(ResolveRebalanceInventoryTopic::getName());
+
+        $this->listener->onFlush($event);
+    }
+
+    public function testProductInventoryIsRebalancedAfterUpdate()
+    {
+        $event = $this->prepareEvent();
+        $uowMock = $this->createMock(UnitOfWork::class);
+
+        $this->entityManager->expects($this->atLeastOnce())
+            ->method('getUnitOfWork')
+            ->willReturn($uowMock);
+
+        $inventoryLevelMock = $this->createMock(InventoryLevel::class);
+        $inventoryItemMock = $this->createMock(InventoryItem::class);
+        $productMock = $this->createMock(Product::class);
+
+
+        $uowMock->expects($this->atLeastOnce())
+            ->method('getScheduledEntityInsertions')
+            ->willReturn([]);
+
+        $uowMock->expects($this->atLeastOnce())
+            ->method('getScheduledEntityUpdates')
+            ->willReturn([$inventoryLevelMock]);
+
+        $inventoryLevelMock->expects($this->once())
+            ->method('getInventoryItem')
+            ->willReturn($inventoryItemMock);
+
+        $inventoryItemMock->expects($this->once())
+            ->method('getProduct')
+            ->willReturn($productMock);
+
+        $productMock->expects($this->exactly(2))
+            ->method('getId')
+            ->willReturn(1);
+
+        $this->producer->expects($this->once())
+            ->method('enableBuffering');
+
+        $this->producer->expects($this->once())
+            ->method('send')
+            ->with(ResolveRebalanceInventoryTopic::getName());
+
+        $this->listener->onFlush($event);
+    }
+
+    /**
+     * @return OnFlushEventArgs
+     */
+    protected function prepareEvent()
+    {
+        return new OnFlushEventArgs($this->entityManager);
     }
 }
