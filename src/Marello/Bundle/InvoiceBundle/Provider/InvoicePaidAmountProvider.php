70be0ffd11f51b83d9dc9629d4fe27aee4426adf
Hotlander <j.casteleijn@labelgoods.nl>
2024-11-29 10:53:19 +0100

    task/MAR10001-1626: - Updated services related to adding payments via order view - Removed unused field from installer - Updated workflow to add additional checks for completion - Updated workflow to remove the paid step from workflow and instead use the Add Payment action button - Updated view of selection of PaymentMethods in the form

--- src/Marello/Bundle/InvoiceBundle/Provider/InvoicePaidAmountProvider.php
+++ src/Marello/Bundle/InvoiceBundle/Provider/InvoicePaidAmountProvider.php
@@ -2,17 +2,95 @@
 
 namespace Marello\Bundle\InvoiceBundle\Provider;
 
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+
+use Marello\Bundle\OrderBundle\Entity\Order;
+use Marello\Bundle\InvoiceBundle\Entity\Invoice;
 use Marello\Bundle\InvoiceBundle\Entity\AbstractInvoice;
 
 class InvoicePaidAmountProvider
 {
-    public function getPaidAmount(AbstractInvoice $entity)
+    /** @var DoctrineHelper $doctrineHelper */
+    private $doctrineHelper;
+
+    /**
+     * @param AbstractInvoice $entity
+     * @return float
+     */
+    public function getPaidAmount(AbstractInvoice $entity): float
     {
         $amount = 0.0;
         foreach ($entity->getPayments() as $payment) {
-            $amount += $payment->getTotalPaid();
+            $amount += (float)$payment->getTotalPaid();
         }
 
-        return $amount;
+        return $this->formatAmount($amount);
+    }
+
+    /**
+     * @param object $entity
+     * @return float|null
+     */
+    public function getPaidAmountForEntity(object $entity): ?float
+    {
+        if ($entity instanceof Order) {
+            $repo = $this->doctrineHelper->getEntityRepositoryForClass(AbstractInvoice::class);
+            $invoices = $repo->findBy([
+                'order' => $entity->getId(),
+                'invoiceType' => Invoice::INVOICE_TYPE
+            ]);
+
+            $totalPaidAmount = 0.0;
+            foreach ($invoices as $invoice) {
+                $totalPaidAmount += $this->getPaidAmount($invoice);
+            }
+
+            return $this->formatAmount($totalPaidAmount);
+        }
+
+        if ($entity instanceof AbstractInvoice) {
+            return $this->getPaidAmount($entity);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param object $entity
+     * @return bool
+     */
+    public function isInvoiceAvailable(object $entity): bool
+    {
+        $isInvoiceAvailable = false;
+        if ($entity instanceof Order) {
+            $repo = $this->doctrineHelper->getEntityRepositoryForClass(AbstractInvoice::class);
+            $invoices = $repo->findBy([
+                'order' => $entity->getId(),
+                'invoiceType' => Invoice::INVOICE_TYPE
+            ]);
+            if (!empty($invoices)) {
+                $isInvoiceAvailable = true;
+            }
+        }
+
+        return $isInvoiceAvailable;
+    }
+
+    /**
+     * @param $amount
+     * @return float
+     */
+    protected function formatAmount($amount): float
+    {
+        return number_format($amount, 4);
+    }
+
+    /**
+     * @param DoctrineHelper $doctrineHelper
+     * @return void
+     */
+    public function setDoctrineHelper(DoctrineHelper $doctrineHelper): void
+    {
+        $this->doctrineHelper = $doctrineHelper;
     }
 }
