bf79a6794ee8047212a190cd6a13697505f28a88
Hotlander <j.casteleijn@labelgoods.nl>
2025-02-17 17:11:11 +0100

    6.0: - fix unmerged

--- src/Marello/Bundle/OrderBundle/Migrations/Schema/v3_1_10/FixOrderBundleMigrationsPart2.php
+++ src/Marello/Bundle/OrderBundle/Migrations/Schema/v3_1_10/FixOrderBundleMigrationsPart2.php
@@ -58,7 +58,7 @@ class FixOrderBundleMigrationsPart2 implements Migration, OrderedMigrationInterf
     protected function createUserOwnership(Schema $schema, QueryBag $queries, Table $table, string $entity): void
     {
         if ($table->hasColumn('user_owner_id')) {
-            if (!$table->hasIndex('idx_a619dd649eb185f9')) {
+            if (!$table->hasIndex('idx_a619dd649eb185f9') || !$table->hasIndex('idx_1118665c9eb185f9')) {
                 $table->addIndex(['user_owner_id']);
             }
 
