a99de9cf05d5e04aeb2e83dbf132ac277ae6e184
Hotlander <j.casteleijn@labelgoods.nl>
2025-09-26 17:00:03 +0200

    feature/MAR10001-1760: - Cleanup some services - Added oauth client creation on customer create

--- src/Marello/Bundle/CustomerBundle/EventListener/Entity/CustomerEventListener.php
+++ src/Marello/Bundle/CustomerBundle/EventListener/Entity/CustomerEventListener.php
@@ -5,11 +5,23 @@ namespace  Marello\Bundle\CustomerBundle\EventListener\Entity;
 use Doctrine\ORM\UnitOfWork;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+use Oro\Bundle\OAuth2ServerBundle\Entity\Client;
+use Oro\Bundle\OAuth2ServerBundle\Entity\Manager\ClientManager;
 
 use Marello\Bundle\CustomerBundle\Entity\Customer;
 
 class CustomerEventListener
 {
+    private $customersInserted = [];
+
+    public function __construct(
+        private ClientManager $clientManager
+    ) {
+    }
+
     /**
      * Handle incoming event
      * @param OnFlushEventArgs $eventArgs
@@ -39,6 +51,50 @@ class CustomerEventListener
         // make sure email is lower cased when saved...
         $entity->setEmail(mb_strtolower($entity->getEmail()));
         $entity->setEmailLowercase(mb_strtolower($entity->getEmail()));
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+        if ($entity instanceof Customer) {
+            if ($entity->isEnabled()) {
+                $this->customersInserted[] = $entity;
+            }
+        }
+    }
+
+    public function postFlush(PostFlushEventArgs $args): void
+    {
+        if (!empty($this->customersInserted)) {
+            $entities = $this->customersInserted;
+            unset($this->customersInserted);
+        }
+
+        /** @var Customer $customer */
+        foreach ($entities as $customer) {
+            $this->createCustomerOAuthClient($customer);
+        }
+    }
+
+    /**
+     * @param Customer $entity
+     */
+    protected function createCustomerOAuthClient(Customer $entity)
+    {
+        if ($entity->isEnabled()) {
+            $client = new Client();
+            $client
+                ->setName($entity->getEmailLowercase())
+                ->setOwnerEntity(Customer::class, $entity->getId())
+                ->setActive(true)
+                ->setGrants(['client_credentials'])
+                ->setOrganization($entity->getOrganization())
+                ->setFrontend(true);
+            $this->clientManager->updateClient($client);
+        }
     }
 
     /**
