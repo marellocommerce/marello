89701f70edffb0db7c2d83da737e2ecceb3f6980
Hotlander <j.casteleijn@labelgoods.nl>
2026-01-06 15:44:11 +0100

    task/MAR10001-0-quantity_in_unit: - Added quantity In unit to InventoryItem

--- src/Marello/Bundle/InventoryBundle/Migrations/Schema/MarelloInventoryBundleInstaller.php
+++ src/Marello/Bundle/InventoryBundle/Migrations/Schema/MarelloInventoryBundleInstaller.php
@@ -32,7 +32,7 @@ class MarelloInventoryBundleInstaller implements Installation, ExtendExtensionAw
      */
     public function getMigrationVersion()
     {
-        return 'v2_8';
+        return 'v2_8_1';
     }
 
     /**
@@ -117,6 +117,7 @@ class MarelloInventoryBundleInstaller implements Installation, ExtendExtensionAw
                 'extend' => ['owner' => ExtendScope::OWNER_SYSTEM],
             ]
         );
+        $table->addColumn('qty_in_unit', 'float', ['notnull' => false]);
         $table->addColumn('on_hand_promise', 'integer', ['notnull' => false]);
         $table->addColumn('drop_ship_promise', 'integer', ['notnull' => false]);
         $table->addColumn('back_order_promise', 'integer', ['notnull' => false]);
