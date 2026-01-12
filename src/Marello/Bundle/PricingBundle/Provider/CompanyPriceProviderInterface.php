ca23084726cb9643e0aa07329d4ee4d2812d30f5
Hotlander <j.casteleijn@labelgoods.nl>
2026-01-12 14:38:23 +0100

    feature/MAR10001-0-order-portal: - Updated ShoppingListUpdate logic for when shoppinglistitems have been added to the cart - Added different format for prices that are coming from the advancedpricelist to include more data - Added event_dispatcher to fire event when updating shoppinglist - Added additional check for when using incorrect configured provider for advanced pricelist

--- src/Marello/Bundle/PricingBundle/Provider/CompanyPriceProviderInterface.php
+++ src/Marello/Bundle/PricingBundle/Provider/CompanyPriceProviderInterface.php
@@ -16,7 +16,7 @@ interface CompanyPriceProviderInterface
     public function getProductPrice(Product $product, string $currency, ?Company $company = null): array;
 
     /**
-     * @param array $products
+     * @param array $products array of product SKU's
      * @param string $currency
      * @param Company|null $company
      * @return array
