29e66b99955f7ad5637089725272badfa09b85a7
Hotlander <j.casteleijn@labelgoods.nl>
2026-02-05 16:49:27 +0100

    feature/MAR10001-1720: - Added additional logic for re-sending invoice emails to either the customer, company or both

--- src/Marello/Bundle/InvoiceBundle/Form/Type/CompanyEmailCheckboxType.php
+++ src/Marello/Bundle/InvoiceBundle/Form/Type/CompanyEmailCheckboxType.php
@@ -56,7 +56,10 @@ class CompanyEmailCheckboxType extends AbstractType
         if (isset($options['relatedEntity'])) {
             $entity = $options['relatedEntity'];
             $view->vars['show_field'] = $this->getIsShowField($entity);
+<<<<<<< HEAD
             $view->vars['relatedEntityEmail'] = $this->getRelatedEntityEmail($entity);
+=======
+>>>>>>> a5f8647d0 (feature/MAR10001-1720:)
         }
     }
 
@@ -78,6 +81,7 @@ class CompanyEmailCheckboxType extends AbstractType
         return $showField;
     }
 
+<<<<<<< HEAD
     /**
      * @param $entity
      * @return bool
@@ -94,6 +98,8 @@ class CompanyEmailCheckboxType extends AbstractType
         return $email;
     }
 
+=======
+>>>>>>> a5f8647d0 (feature/MAR10001-1720:)
     public function getParent()
     {
         return BaseCheckboxType::class;
