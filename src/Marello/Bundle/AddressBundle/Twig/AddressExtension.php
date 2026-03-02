28d8b673c25e6ff445f10e1443039aaed5d127af
Hotlander <j.casteleijn@labelgoods.nl>
2026-03-02 14:09:47 +0100

    feature/MAR10001-0-order-portal-plus-equipment: - Added copy for address rendering in address extension, only when the address is a typed address

--- src/Marello/Bundle/AddressBundle/Twig/AddressExtension.php
+++ src/Marello/Bundle/AddressBundle/Twig/AddressExtension.php
@@ -2,10 +2,14 @@
 
 namespace Marello\Bundle\AddressBundle\Twig;
 
-use Oro\Bundle\LocaleBundle\Formatter\AddressFormatter;
-use Oro\Bundle\LocaleBundle\Model\AddressInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+
+use Oro\Bundle\LocaleBundle\Model\AddressInterface;
+use Oro\Bundle\LocaleBundle\Formatter\AddressFormatter;
+
+use Marello\Bundle\AddressBundle\Entity\MarelloAddress;
+use Marello\Bundle\AddressBundle\Entity\MarelloTypedAddress;
 
 class AddressExtension extends AbstractExtension
 {
@@ -39,15 +43,40 @@ class AddressExtension extends AbstractExtension
     /**
      * Formats address according to locale settings.
      *
-     * @param AddressInterface $address
+     * @param                  $address
      * @param string|null      $country
      * @param string           $newLineSeparator
      *
      * @return string
      */
-    public function formatAddress(AddressInterface $address, $country = null, $newLineSeparator = "\n")
+    public function formatAddress($address, $country = null, $newLineSeparator = "\n")
     {
+        if ($address instanceof MarelloTypedAddress::class) {
+            // tmp copy typed address into Address
+            $address = $this->copyAddress($address);
+        }
         return $this->addressFormatter->format($address, $country, $newLineSeparator);
+    }
+
+    private function copyAddress(MarelloTypedAddress $typedAddress)
+    {
+        $address = new MarelloAddress();
+        $address
+            ->setCountry($typedAddress->getCountry())
+            ->setRegion($typedAddress->getRegion())
+            ->setNamePrefix($typedAddress->getNamePrefix())
+            ->setFirstName($typedAddress->getFirstName())
+            ->setMiddleName($typedAddress->getMiddleName())
+            ->setLastName($typedAddress->getLastName())
+            ->setNameSuffix($typedAddress->getNameSuffix())
+            ->setCity($typedAddress->getCity())
+            ->setPostalCode($typedAddress->getPostalCode())
+            ->setStreet($typedAddress->getStreet())
+            ->setStreet2($typedAddress->getStreet2())
+            ->setPhone($typedAddress->getPhone())
+            ->setCompany($typedAddress->getCompany());
+
+        return $address;
     }
 
     /**
