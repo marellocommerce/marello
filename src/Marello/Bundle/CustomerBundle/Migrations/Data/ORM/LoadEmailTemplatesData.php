d268047388c6c6d93565abcbd333c03a72ed3364
Hotlander <j.casteleijn@labelgoods.nl>
2026-07-01 10:52:08 +0200

    feature/MAR10001-0-order-portal-plus-equipment:  - Added updated configuration for customer email templates (welcome mail and password reset)

---
+++ src/Marello/Bundle/CustomerBundle/Migrations/Data/ORM/LoadEmailTemplatesData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Marello\Bundle\CustomerBundle\Migrations\Data\ORM;
+
+use Doctrine\Persistence\ObjectManager;
+
+use Oro\Bundle\EmailBundle\Entity\EmailTemplate;
+use Oro\Bundle\MigrationBundle\Fixture\VersionedFixtureInterface;
+use Oro\Bundle\EmailBundle\Migrations\Data\ORM\AbstractEmailFixture;
+
+class LoadEmailTemplatesData extends AbstractEmailFixture implements
+    VersionedFixtureInterface
+{
+    /**
+     * Return path to email templates
+     *
+     * @return string
+     */
+    public function getEmailsDir()
+    {
+        return $this->container
+            ->get('kernel')
+            ->locateResource('@MarelloCustomerBundle/Migrations/Data/ORM/data/emails');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function findExistingTemplate(ObjectManager $manager, array $template)
+    {
+        $name = $template['params']['name'];
+        if (empty($name)) {
+            return null;
+        }
+
+        return $manager->getRepository(EmailTemplate::class)->findOneBy([
+            'name' => $template['params']['name'],
+            'entityName' => 'Marello\Bundle\CustomerBundle\Entity\Customer',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '1.3';
+    }
+}
